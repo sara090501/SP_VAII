8369ee0406107f6127e664600920e8f59a74dc0a
geratova.sara <geratova.sara@gmail.com>
2022-11-18 22:42:53 +0100

    adding products done

--- App/Views/Offer/add.view.php
+++ App/Views/Offer/add.view.php
@@ -16,17 +16,24 @@
 </head>
 
 <body>
-<div class="container">
     <form action="?c=offer&a=add" method="post">
         <div class="form-group">
-            <label for="exampleFormControlInput1">Vaše meno</label>
-            <input name="title" type="text" class="form-control" id="exampleFormControlInput1">
+            <label for="exampleFormControlInput1">Názov</label>
+            <input name="name" type="text" class="form-control" id="exampleFormControlInput1" >
+        </div>
+
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Popis</label>
+            <textarea name="description" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
         </div>
         <div class="form-group">
-            <label for="exampleFormControlTextarea1">Recenzia</label>
-            <textarea name="text" class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <label for="exampleFormControlTextarea1">Cesta k fotke</label>
+            <textarea name="imgpath" class="form-control" id="exampleFormControlTextarea2" rows="3"></textarea>
         </div>
-        <button type="submit" class="btn btn-sm btn-outline-secondary submit button">Pokus</button>
+        <div class="form-group">
+            <label for="exampleFormControlTextarea1">Cena produktu</label>
+            <textarea name="price" class="form-control" id="exampleFormControlTextarea3" rows="3"></textarea>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
     </form>
-</div>
 </body>
